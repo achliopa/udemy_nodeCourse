50d038a9a158bdb351da77895c1e84371e13f287
Sakis Chliopanos <athchliopanos@gmail.com>
2017-10-18 09:18:16 +0000

    made it heroku ready for deploy

--- mycode/section5/node-web-server/server.js
+++ mycode/section5/node-web-server/server.js
@@ -1,25 +1,67 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+var ip = process.env.IP || 'localhost';
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+// if here bypasses the middleware had to put it below in order of code execution
+// app.use(express.static(__dirname + '/public'));
+
+// middleware
+app.use((req, res, next) => {
+   var now = new Date().toString();
+   var log = `${now}: ${req.method} ${req.url}`;
+   console.log(log);
+   fs.appendFile('server.log', log + '\n', (err) => {
+       if(err)  {
+           console.log('Uanble to append to server.log');
+       }
+   });
+   next();
+});
+
+app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear(); 
+});
+
+hbs.registerHelper('screamIt', (text) => {
+   return  text.toUpperCase();
+});
+
 app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', (req,res) => {
     // res.send('<h1>Hello Express</h1>');
-    res.send({
-        name: 'Sakis',
-        likes: [
-            'Programming',
-            'Jiu-Jitsu',
-            'Dogs',
-            'MTB',
-            'Post-Rock'
-            ]
-    })
+    // res.send({
+    //     name: 'Sakis',
+    //     likes: [
+    //         'Programming',
+    //         'Jiu-Jitsu',
+    //         'Dogs',
+    //         'MTB',
+    //         'Post-Rock'
+    //         ]
+    // })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        message: 'Welcome to our Site!'
+    });
 });
 
 app.get('/about', (req,res) => {
-    res.send('About Page')
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
 app.get('/bad', (req,res) => {
@@ -28,7 +70,6 @@ app.get('/bad', (req,res) => {
     });
 })
 
-// app.listen(3000);
-app.listen(process.env.PORT, process.env.IP, () => {
+app.listen(port, ip, () => {
     console.log('Server is Up at port 3000');
 });
