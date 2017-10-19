88322d4076283e9d072bf573791e08cabce7ae0b
achliopa <athchliopanos@gmail.com>
2017-10-20 01:25:10 +0300

    up to lesson 70

---
+++ mycode/section7/node-todo-api/server/server.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+mongoose.promise = global.promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+	text: {
+		type: String
+	},
+	completed: {
+		type: Boolean
+	},
+	completedAt: {
+		type: Number
+	}
+});
+
+// var newTodo = new Todo({
+// 	text: 'Cook dinner'
+// });
+
+var newTodo = new Todo({
+	text: 'Walk the dog',
+	completed: true,
+	completedAt: Date.now()
+});
+
+newTodo.save().then((doc) => {
+	console.log('Saved todo',doc);
+}, (err) => {
+	console.log('Unable to save todo');
+});
