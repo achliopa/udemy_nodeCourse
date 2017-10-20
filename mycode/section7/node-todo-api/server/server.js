301780b8e794d6f06d80fd3687a6c948712d8114
achliopa <athchliopanos@gmail.com>
2017-10-20 21:45:54 +0300

    added POST route to RESTful  API

--- mycode/section7/node-todo-api/server/server.js
+++ mycode/section7/node-todo-api/server/server.js
@@ -14,11 +14,15 @@ app.use(bodyParser.json());
 
 //POST route
 app.post('/todos', (req,res) => {
-	console.log(req.body);
-});
+	var todo = new Todo({
+		text: req.body.text
+	});
 
-app.get('/', (req,res) => {
-	res.send('Hi');
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	});
 });
 
 app.listen(port, ip, () => {
