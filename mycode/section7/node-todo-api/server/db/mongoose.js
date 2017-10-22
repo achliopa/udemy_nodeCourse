9bbb6543a42dc4b64bfc939cb9cd0003cf924e10
achliopa <athchliopanos@gmail.com>
2017-10-22 14:55:17 +0300

    finished chapter 7 added patch with test

--- mycode/section7/node-todo-api/server/db/mongoose.js
+++ mycode/section7/node-todo-api/server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODV_URI || 'mongodb://localhost/TodoApp', {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 mongoose.Promise = global.Promise;
 
 module.exports = {mongoose};
--- mycode/section7/node-todo-api/server/server.js
+++ mycode/section7/node-todo-api/server/server.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+require('./config/config');
 
+const _= require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
-const port = process.env.PORT || 3000;
-const ip = process.env.IP || 'localhost';
 
 var app = express();
 
@@ -63,8 +63,33 @@ app.delete('/todos/:id', (req,res) => {
 	}).catch((e) => res.status(400).send());
 });
 
-app.listen(port, ip, () => {
-	console.log('App is listening on port: ', port);
+
+// PATCH /todos/:id
+app.patch('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text','completed']);
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404),sebd();
+	}
+
+	if(_.isBoolean(body.completed) && (body.completed)) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => res.status(400).send());
+});
+
+app.listen(process.env.PORT, process.env.IP, () => {
+	console.log('App is listening on port: ', process.env.PORT);
 });
 
 module.exports = {app};
