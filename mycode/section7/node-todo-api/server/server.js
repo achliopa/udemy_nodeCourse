96ee71ade4069c97cb5f9abed9a4a57f8dcfdccb
achliopa <athchliopanos@gmail.com>
2017-10-22 10:03:20 +0300

    added DELETE route with tests

--- mycode/section7/node-todo-api/server/server.js
+++ mycode/section7/node-todo-api/server/server.js
@@ -49,6 +49,20 @@ app.get('/todos/:id', (req,res) => {
 	}).catch((e) => res.status(400).send());
 });
 
+//DELETE todo 
+app.delete('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => res.status(400).send());
+});
+
 app.listen(port, ip, () => {
 	console.log('App is listening on port: ', port);
 });
