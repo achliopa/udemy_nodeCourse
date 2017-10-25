156e3d4989e0d40511cd3195cdc9115ec0561726
achliopa <athchliopanos@gmail.com>
2017-10-25 16:37:21 +0300

    added connect/disconnect event listeners to socket.io srv/client

--- mycode/section9/node-chat-app/server/server.js
+++ mycode/section9/node-chat-app/server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
+const port = process.env.PORT || 3000;
+const ip = process.env.IP || 'localhost';
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+	console.log('New user connected');
 
-app.listen(3000, ()=> {
-	console.log('App is listening in port 3000');
+	socket.on('disconnect', () => {
+		console.log('Client disconnected');
+	});
+})
+
+server.listen(port, ip, ()=> {
+	console.log(`Server is Up on http://${ip}:${port}/`);
 });
