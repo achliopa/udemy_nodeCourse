38df3ea18af4ab0055660f41a4b6d668eb3a1234
achliopa <athchliopanos@gmail.com>
2017-10-25 12:04:17 +0300

    made all todo routes private, adapteed test cases

--- mycode/section8/node-todo-api/server/tests/seed/seed.js
+++ mycode/section8/node-todo-api/server/tests/seed/seed.js
@@ -3,16 +3,6 @@ const jwt = require('jsonwebtoken');
 const {Todo} = require('./../../models/todo');
 const {User} = require('./../../models/user');
 
-const todos = [{
-	_id: new ObjectID(),
-	text: 'First test todo'
-}, {
-	_id: new ObjectID(),
-	text: 'Second test todo',
-	completed: true,
-	completedAt: 2333
-}];
-
 const userOneId = new ObjectID();
 const userTwoId = new ObjectID();
 const users = [{
@@ -26,10 +16,24 @@ const users = [{
 }, {
 	_id: userTwoId,
 	email: 'jen@example.com',
-	password: 'userTwoPass'
+	password: 'userTwoPass',
+	tokens: [{
+		access: 'auth',
+		token: jwt.sign({_id: userTwoId.toHexString(), access: 'auth'}, 'abc123').toString()
+	}]
 }];
 
-
+const todos = [{
+	_id: new ObjectID(),
+	text: 'First test todo',
+	_creator: userOneId
+}, {
+	_id: new ObjectID(),
+	text: 'Second test todo',
+	completed: true,
+	completedAt: 2333,
+	_creator: userTwoId
+}];
 
 const populateTodos = (done) => {
 	Todo.remove({}).then(() => {
