7a31569e4d2ed3ddaf44be3ed50eb7f91cb92253
Sakis Chliopanos <athchliopanos@gmail.com>
2017-10-27 07:42:44 +0000

    added geolocation functionality to chat app and tests

--- mycode/section9/node-chat-app/server/utils/message.test.js
+++ mycode/section9/node-chat-app/server/utils/message.test.js
@@ -1,15 +1,27 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', ()=> {
         var from = 'Andrew';
         var text = 'Hello there';
-        var res = generateMessage(from,text)
+        var res = generateMessage(from,text);
         // expect(res.from).toBe(from);
         // expect(res.text).toBe(text);
         expect(res).toInclude({from,text});
         expect(typeof res.createdAt).toBe('number');
     });
 });
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location message object', () => {
+       var from = 'Sakis';
+       var latitude = 40.7128;
+       var longitude = 74.0060;
+       var res = generateLocationMessage(from, latitude, longitude);
+       expect(res).toInclude({from});
+       expect(typeof res.createdAt).toBe('number');
+       expect(res.url).toInclude(latitude,longitude);
+    });
+})
