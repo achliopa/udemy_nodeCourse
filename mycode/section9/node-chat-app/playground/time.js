5862a959921406d8c44ae83ad7a48ec6f67ce59f
Sakis Chliopanos <athchliopanos@gmail.com>
2017-10-27 11:29:31 +0000

    added formatted timestamp messages

---
+++ mycode/section9/node-chat-app/playground/time.js
@@ -0,0 +1,20 @@
+// Jan 1st 1970 00:00:00 am
+const moment = require('moment');
+
+
+// var date = moment();
+// date.add(100, 'year').subtract(7,'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+// var date = moment(new Date().getTime());
+// console.log(date.format('h:mm a'));
+
+// var createdAt = 1223233233334;
+
+// var date = moment(createdAt);
+// console.log(date.fromNow());
+
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
