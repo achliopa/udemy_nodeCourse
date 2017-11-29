ffdb05414f88eac55fb684f26d050350a1046bad
Sakis Chliopanos <athchliopanos@gmail.com>
2017-11-29 14:09:58 +0000

    async await

---
+++ mycode/section10/async-await/app-promises.js
@@ -0,0 +1,102 @@
+const users = [{
+    id: 1,
+    name: 'Andrew',
+    schoolId: 101
+}, {
+    id: 2,
+    name: 'Jessica',
+    schoolId: 999
+}];
+
+const grades = [{
+    id: 1,
+    schoolId: 101,
+    grade: 86
+}, {
+    id: 2,
+    schoolId: 999,
+    grade: 100
+}, {
+    id: 3,
+    schoolId: 101,
+    grade: 80
+}];
+
+const getUser = (id) => {
+    return new Promise((resolve,reject) => {
+        // const user = users.find((user) => {
+        //     return user.id === id;
+        // });
+        // one liner
+        const user = users.find((user) => user.id === id);
+        
+        if(user) {
+            resolve(user);
+        } else {
+            reject(`Unable to find User with id of ${id}`);
+        }
+    });
+};
+
+const getGrades = (schoolId) => {
+  return new Promise((resolve,reject) => {
+      resolve(grades.filter((grade) => grade.schoolId === schoolId));
+  });  
+};
+
+
+// getUser(21).then((user) => {
+//     console.log(user);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+// getGrades(99).then((grades) => {
+//     console.log(grades);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+
+// chaining promises, nesting
+const getStatus = (userId) => {
+    let user;
+    return getUser(userId).then((tempUser) => {
+        user = tempUser;
+        return getGrades(user.schoolId);
+    }).then((grades) => {
+        let average = 0;
+        if(grades.length > 0) {
+            average = grades.map((grade) => grade.grade).reduce((a, b) => a + b) / grades.length;            
+        }
+        return `${user.name} has an average of ${average}%`;
+
+    });
+}
+
+// async/await basics
+
+// async functions always return promises
+
+const getStatusAlt = async (userId) => {
+    return 'Mike';
+};
+
+// identical to
+
+// const getStatusAlt = () => {
+//     return new Promise((resolve,reject) => {
+//         resolve('Mike');
+//     });
+// };
+
+getStatusAlt().then((name) => {
+    console.log(name);
+});
+
+
+// getStatus(1).then((status) => {
+//     console.log(status);
+// }).catch((e) => {
+//     console.log(e);
+// });
